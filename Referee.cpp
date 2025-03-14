c5b5feb0084b1670986e0b865056c28907c01ef5
henryn3005 <ineverdie69@hotmail.com>
2025-03-14 17:19:52 +1100

    update2

--- Referee.cpp
+++ Referee.cpp
@@ -3,21 +3,27 @@
 Referee::Referee() {}
 
 Player* Referee::refGame(Player* player1, Player* player2) {
-    Move* player1move = player1->makeMove();
-    Move* player2move = player2->makeMove();
+    Move* p1move = player1->makeMove();
+    Move* p2move = player2->makeMove();
 
-    std::string p1move = player1->getName();
-    std::string p2move = player2->getName();
+    std::string move1 = p1move->getName();
+    std::string move2 = p2move->getName();
 
-    if ((p1move == "Rock" && p2move == "Scissors") || (p1move == "Rock" && p2move == "Robot")) return player1;
-    if ((p1move == "Scissors" && p2move == "Paper") || (p1move == "Scissors" && p2move == "Monkey")) return player1;
-    if ((p1move == "Paper" && p2move == "Rock") || (p1move == "Paper" && p2move == "Pirate")) return player1;
-    if ((p1move == "Monkey" && p2move == "Robot") || (p1move == "Monkey" && p2move == "Zombie")) return player1;
-    if ((p1move == "Robot" && p2move == "Ninja") || (p1move == "Robot" && p2move == "Monkey")) return player1;
-    if ((p1move == "Pirate" && p2move == "Zombie") || (p1move == "Pirate" && p2move == "Robot")) return player1;
-    if ((p1move == "Ninja" && p2move == "Pirate") || (p1move == "Ninja" && p2move == "Monkey")) return player1;
-    if ((p1move == "Zombie" && p2move == "Pirate") || (p1move == "Zombie" && p2move == "Ninja")) return player1;
+    if (move1 == move2) {
+        return nullptr;
+    }
 
-    return nullptr;
+    if ((move1 == "Rock" && (move2 == "Scissors" || move2 == "Robot")) ||
+        (move1 == "Scissors" && (move2 == "Paper" || move2 == "Monkey")) ||
+        (move1 == "Paper" && (move2 == "Rock" || move2 == "Pirate")) ||
+        (move1 == "Robot" && (move2 == "Monkey" || move2 == "Zombie")) ||
+        (move1 == "Monkey" && (move2 == "Pirate" || move2 == "Ninja")) ||
+        (move1 == "Pirate" && (move2 == "Zombie" || move2 == "Scissors")) ||
+        (move1 == "Ninja" && (move2 == "Robot" || move2 == "Paper")) ||
+        (move1 == "Zombie" && (move2 == "Ninja" || move2 == "Rock"))) {
+        return player1;
+    }
+
+    return player2;
 }
 
